70f1963b1bfc1434c86826309bbcbbc85fe82c24
Santiago Amarilla <8smat88@gmail.com>
2024-07-13 11:56:39 -0400

    Agregar comentario de paso faltante en login

--- AppChat/src/app/pages/login/login.page.ts
+++ AppChat/src/app/pages/login/login.page.ts
@@ -9,14 +9,17 @@ import { Router } from '@angular/router';
 export class LoginPage {
   email: string | undefined;
   password: string | undefined;
-  username: any;
+  username: string | undefined;
 
   constructor(private router: Router) {}
 
   login() {
     // Aquí puedes añadir la lógica para verificar las credenciales del usuario
     console.log('Intentando iniciar sesión con:', this.email, this.password);
+
+    //TODO acá deberíamos recuperar el nombre del usuario y guardarlo en vez del email
     localStorage.setItem("username", this.email || "");
+    
     // Si la verificación es correcta, redirige a la selección de sala de chat
     this.router.navigate(['/chat-selection']);
   }
